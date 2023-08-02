65dccddd34af007dbc4eca7784bd2eb5c35c4564
raj075512 <raj075512@gmail.com>
2023-08-02 15:31:09 +0530

    makinf of main body landing section of queston tage or problem set

---
+++ pages/QuestionSet/QuestionSetHeading.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Link from 'next/link'
+import Navbar from '../components/Navbar'
+
+function QuestionSetHeading() {
+  return (
+    <div className="flex items-center justify-space  bg-gray-300">
+      
+      
+
+       
+    </div>
+  )
+}
+
+export default QuestionSetHeading
