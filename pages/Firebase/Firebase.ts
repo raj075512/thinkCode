9800372a6fec8f20d8070824f6479810f2442061
raj075512 <raj075512@gmail.com>
2023-08-08 17:08:26 +0530

    firbasse set complete

--- pages/Firebase/Firebase.ts
+++ pages/Firebase/Firebase.ts
@@ -1,19 +1,28 @@
-import { initializeApp, getApp, getApps } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
 
+
+
+// Import the functions you need from the SDKs you need
+import { getApp, getApps, initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore"
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey:process.env.FIREBASE_APIKEY,
+  authDomain:process.env.FIREBASE_AUTHDOMAIN,
+  projectId:process.env.FIREBASE_PROJECTID,
+  storageBucket:process.env.FIREBASE_STORAGEBUCKET,
+  messagingSenderId:process.env.FIREBASE_MESSAGINGSENDERID,
+  appId:process.env.FIREBASE_APPID,
+  measurementId:process.env.FIREBASE_MEASUREMENTID
 };
 
-const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+// Initialize Firebase
+const app = !getApps.length? initializeApp(firebaseConfig) :getApp() ;
+const auth=getAuth(app);
+const fireStore =getFirestore(app);
 
-const auth = getAuth(app);
-const firestore = getFirestore(app);
-
-export { auth, firestore, app };
+export {auth,fireStore,app};
